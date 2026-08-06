99db4b056d76ef08a28b87862608ffadf44eb7ee
mahesh855588 <unity855588@gmail.com>
2026-08-06 19:28:16 +0530

    practice operators

---
+++ PYTHON/Practice/Operators.py
@@ -0,0 +1,38 @@
+#exponentiation
+#exponentiation is right assosiative(left to right)
+# 2**(3**2) = 2**9 =512
+print(2**3**2) 
+
+#arithmetic operators
+a=10
+b=29
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(b%a)
+
+
+#comparision operators
+a=20
+b=77
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+#logical operators
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not b)
+
+#assignment operators
+a=5
+b=7
+print(a+=8)
+
